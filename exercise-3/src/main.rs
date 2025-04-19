1ce09de76d191401956b09455f6bd36660090cd1
SRL132 <srocalag@gmail.com>
2025-04-19 13:49:30 +0200

    solve exercise 3

--- exercise-3/src/main.rs
+++ exercise-3/src/main.rs
@@ -15,17 +15,34 @@ pub fn deposit(ctx: Context<Deposit>, collat: u64) -> Result<()> {
 
     Ok(())
 }
-
 //exchange_rate.deposit_rate is an u64
 //DECIMALS_SCALAR value is not given. It is an u128 which may have any value except 0.
 
 //Explain what the function does
-//Classic Deposit function where the user deposits collateral tokens and receives shares tokens in return
+//Classic Deposit function where the user deposits collateral tokens and receives shares tokens in return according to the deposit rate
 
 //What could go wrong?
 //amt is downcasted to u64, if the rate is too high it could overflow, causing the user to get less shares than expected
 
 //How to fix it?
 //Use u128 for amt and rate, and use a constant for DECIMALS_SCALAR
+//assuming mint_to expects to get u64, add overflow check before downcasting
 
+pub fn depositSolution(ctx: Context<Deposit>, collat: u64) -> Result<()> {
+    let rate = exchange_rate.deposit_rate; //u64
+    let rate = exchange_rate.deposit_rate as u128;
+    let amt = collat as u128 * rate / DECIMALS_SCALAR; 
+    
+    //add overflow check
+    if amt > u64::MAX {
+        return Err(Error::AMOUNT_TOO_HIGH);
+    }
 
+            // transfer(token, from, to, amount)
+    token::transfer(collateral_token, ctx.caller, ctx.this, collat)?;
+
+            // mint_to(token, to, amount)
+    token::mint_to(shares_token, ctx.caller, amt as u64)?;
+
+    Ok(())
+}
---
+++ exercise-3/target/debug/deps/exercise_3-daafca887b2d156a.d
@@ -0,0 +1,5 @@
+/Users/sergirocalaguna/Desktop/study/rust-week-1/exercise-3/target/debug/deps/exercise_3-daafca887b2d156a: src/main.rs
+
+/Users/sergirocalaguna/Desktop/study/rust-week-1/exercise-3/target/debug/deps/exercise_3-daafca887b2d156a.d: src/main.rs
+
+src/main.rs:
