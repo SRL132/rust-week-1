1c97aed765559fb8192353e74243723f66739775
SRL132 <srocalag@gmail.com>
2025-04-19 13:50:22 +0200

    finish

---
+++ exercise-2/target/debug/deps/exercise_2-08521adfc0be2d15.d
@@ -0,0 +1,5 @@
+/Users/sergirocalaguna/Desktop/study/rust-week-1/exercise-2/target/debug/deps/exercise_2-08521adfc0be2d15: src/main.rs
+
+/Users/sergirocalaguna/Desktop/study/rust-week-1/exercise-2/target/debug/deps/exercise_2-08521adfc0be2d15.d: src/main.rs
+
+src/main.rs:
